13e460e75ee1566270f62f627292c1f8439643be
Racksoup <rack.connor1@gmail.com>
2022-08-13 16:21:50 -0400

    Cleaned Frontend

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,6 @@ import React from 'react';
 import './App.scss';
 import Home from './UI/Pages/Home/Home.jsx';
 import AdminLogin from './UI/Pages/AdminLogin/AdminLogin.jsx';
-import UserLogin from './UI/Pages/UserLogin/UserLogin.jsx';
-import CreateUser from './UI/Pages/CreateUser/CreateUser.jsx';
 import Navbar from './UI/Components/Navbar/Navbar.jsx';
 import Footer from './UI/Components/Footer/Footer.jsx';
 
@@ -18,8 +16,6 @@ const App = () => {
         <Routes>
           <Route exact path='/' element={<Home />} />
           <Route exact path='/admin-login' element={<AdminLogin />} />
-          <Route exact path='/user-login' element={<UserLogin />} />
-          <Route exact path='/create-user' element={<CreateUser />} />
         </Routes>
         <Footer />
       </div>
