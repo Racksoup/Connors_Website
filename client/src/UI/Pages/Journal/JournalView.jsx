02c2e3d33946fb15ec56b39c923c7cdd6b17ab15
Racksoup <rack.connor1@gmail.com>
2022-08-14 22:06:57 -0400

    created JournalView

--- client/src/UI/Pages/Journal/JournalView.jsx
+++ client/src/UI/Pages/Journal/JournalView.jsx
@@ -1,8 +1,46 @@
 import React from 'react';
 import './JournalView.scss';
+import { clearJournal, selectJournal } from '../../../Redux/calendarSlice';
+
+import { useSelector } from 'react-redux';
+import { Navigate } from 'react-router-dom';
 
 const JournalView = () => {
-  return <div className='JournalView'>JournalView</div>;
+  const journal = useSelector(selectJournal);
+
+  if (journal.title === '') {
+    return <Navigate to='/journal-entry' />;
+  }
+
+  let textArr = [];
+  if (journal && journal.text) {
+    textArr = journal.text.split('\n\n');
+  }
+  return (
+    <div className='JournalView'>
+      <div className='TitleBox'>
+        <Link to='home' className='Link'>
+          <div className='Btn' onClick={() => clearJournal()}>
+            Back
+          </div>
+        </Link>
+        <p className='Title'>{journal.title}</p>
+        <Link to='journal-entry' className='Link'>
+          <div className='Btn'>Edit</div>
+        </Link>
+      </div>
+      <div className='Content'>
+        <div className='PictureFrame'>
+          <img className='Img' src={`api/journal/image/${journal.image_filename}`} />
+        </div>
+        <div className='Text'>
+          {textArr.map((text) => (
+            <p>{text}</p>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default JournalView;
