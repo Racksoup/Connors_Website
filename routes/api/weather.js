3f04145346da107b653829f836f3cfb3db7367f2
Racksoup <rack.connor1@gmail.com>
2022-08-15 17:11:14 -0400

    getting ready for deploy

--- routes/api/weather.js
+++ routes/api/weather.js
@@ -1,4 +1,7 @@
 const Historical = require('../../models/Historical');
+const WeatherID = process.env.OPEN_WEATHER_MAP_ID;
+const Latitude = process.env.LATITUDE;
+const Longitude = process.env.LONGITUDE;
 
 const axios = require('axios');
 const express = require('express');
@@ -7,7 +10,7 @@ const router = express.Router();
 router.get('/', async (req, res) => {
   try {
     const forecast = await axios.get(
-      'https://api.openweathermap.org/data/2.5/forecast?q=Ottawa,ca&appid=5892035cc2ec047af9b8a3ee8d181525'
+      `https://api.openweathermap.org/data/2.5/forecast?q=Ottawa,ca&appid=${WeatherID}`
     );
     res.json(forecast.data);
   } catch (err) {
@@ -18,7 +21,7 @@ router.get('/', async (req, res) => {
 router.get('/one-call', async (req, res) => {
   try {
     const weather = await axios.get(
-      'https://api.openweathermap.org/data/2.5/onecall?lat=45.46&lon=-75.51&exclude=alerts&appid=5892035cc2ec047af9b8a3ee8d181525'
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${Latitude}&lon=${Longitude}&exclude=alerts&appid=${WeatherID}`
     );
     res.json(weather.data);
   } catch (err) {
@@ -36,7 +39,7 @@ router.get('/historical', async (req, res) => {
       currDate -= 86400;
       allPromises.push(
         await axios.get(
-          `https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=47.53&lon=-77.64&dt=${currDate}&appid=5892035cc2ec047af9b8a3ee8d181525`
+          `https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=${Latitude}&lon=${Longitude}&dt=${currDate}&appid=${WeatherID}`
         )
       );
     }
