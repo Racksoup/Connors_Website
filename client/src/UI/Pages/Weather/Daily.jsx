3cc944c4edc2ecf2399a44ab615f3f221a38da59
Racksoup <rack.connor1@gmail.com>
2022-08-18 01:01:13 -0400

    made daily more better for mobile

--- client/src/UI/Pages/Weather/Daily.jsx
+++ client/src/UI/Pages/Weather/Daily.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Weather.scss';
 import { selectDaily } from '../../../Redux/weatherSlice';
 
@@ -29,30 +29,95 @@ const Daily = () => {
     day.theClouds = <div>&#9728;</div>;
   });
 
+  const [screenSize, getDimension] = useState({
+    dynamicWidth: window.innerWidth,
+    dynamicHeight: window.innerHeight,
+  });
+
+  const setDimension = () => {
+    getDimension({
+      dynamicWidth: window.innerWidth,
+      dynamicHeight: window.innerHeight,
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', setDimension);
+
+    return () => {
+      window.removeEventListener('resize', setDimension);
+    };
+  }, [screenSize]);
+
   if (daily) {
     return (
       <div className='Weather'>
         <h1 className='Title'>Daily</h1>
         <Table bordered variant='dark' style={{ width: '80%', margin: 'auto', marginTop: '20px' }}>
-          <thead>
-            <tr style={{ color: 'white' }}>
-              <th>Day</th>
-              <th>Max Temp</th>
-              <th>Min Temp</th>
-              <th>Pressure</th>
-              <th>Humidity</th>
-              <th>Wind Speed</th>
-              <th>Weather</th>
-              <th>Clouds</th>
-              <th>Rain</th>
-              <th>Pop</th>
-              <th>Sunrise</th>
-              <th>Sunset</th>
-              <th>Moonrise</th>
-              <th>Moonset</th>
-              <th>Moon Phase</th>
-            </tr>
-          </thead>
+          {screenSize.dynamicWidth > 720 ? (
+            <thead>
+              <tr style={{ color: 'white' }}>
+                <th>Day</th>
+                <th>Max Temp</th>
+                <th>Min Temp</th>
+                <th>Pressure</th>
+                <th>Humidity</th>
+                <th>Wind Speed</th>
+                <th>Weather</th>
+                <th>Clouds</th>
+                <th>Rain</th>
+                <th>Pop</th>
+                <th>Sunrise</th>
+                <th>Sunset</th>
+                <th>Moonrise</th>
+                <th>Moonset</th>
+                <th>Moon Phase</th>
+              </tr>
+            </thead>
+          ) : screenSize.dynamicWidth > 600 ? (
+            <thead>
+              <tr style={{ color: 'white' }}>
+                <th>Day</th>
+                <th>Max Temp</th>
+                <th>Min Temp</th>
+                <th>Pressure</th>
+                <th>Humidity</th>
+                <th>Wind Speed</th>
+                <th>Weather</th>
+                <th>Clouds</th>
+                <th>Rain</th>
+                <th>Pop</th>
+                <th>Sunrise</th>
+                <th>Sunset</th>
+                <th>Moon Phase</th>
+              </tr>
+            </thead>
+          ) : screenSize.dynamicWidth > 400 ? (
+            <thead>
+              <tr style={{ color: 'white' }}>
+                <th>Day</th>
+                <th>Max Temp</th>
+                <th>Min Temp</th>
+                <th>Humidity</th>
+                <th>Weather</th>
+                <th>Pop</th>
+                <th>Sunrise</th>
+                <th>Sunset</th>
+                <th>Moon Phase</th>
+              </tr>
+            </thead>
+          ) : (
+            <thead>
+              <tr style={{ color: 'white' }}>
+                <th>Day</th>
+                <th>Max Temp</th>
+                <th>Min Temp</th>
+                <th>Weather</th>
+                <th>Pop</th>
+                <th>Moon Phase</th>
+              </tr>
+            </thead>
+          )}
           <tbody>
             {daily.map((day, i) => {
               if (daily[i].clouds > 32) {
@@ -61,25 +126,71 @@ const Daily = () => {
               if (daily[i].clouds > 66) {
                 day.theClouds = <div>&#9729;</div>;
               }
-              return (
-                <tr style={{ color: 'white' }}>
-                  <td>{day.theDay}</td>
-                  <td>{(day.temp.max - 273.15).toFixed(1)}&#176;</td>
-                  <td>{(day.temp.min - 273.15).toFixed(1)}&#176;</td>
-                  <td>{day.pressure}</td>
-                  <td>{day.humidity}%</td>
-                  <td>{day.wind_speed}</td>
-                  <td>{day.weather[0].main}</td>
-                  <td>{day.theClouds}</td>
-                  <td>{day.rain}</td>
-                  <td>{Math.round(day.pop * 100)}%</td>
-                  <td>{day.theSunrise[0]}</td>
-                  <td>{day.theSunset}</td>
-                  <td>{day.theMoonrise}</td>
-                  <td>{day.theMoonset}</td>
-                  <td>{Math.round(day.moon_phase * 100)}%</td>
-                </tr>
-              );
+
+              if (screenSize.dynamicWidth > 720) {
+                return (
+                  <tr style={{ color: 'white' }}>
+                    <td>{day.theDay}</td>
+                    <td>{(day.temp.max - 273.15).toFixed(1)}&#176;</td>
+                    <td>{(day.temp.min - 273.15).toFixed(1)}&#176;</td>
+                    <td>{day.pressure}</td>
+                    <td>{day.humidity}%</td>
+                    <td>{day.wind_speed}</td>
+                    <td>{day.weather[0].main}</td>
+                    <td>{day.theClouds}</td>
+                    <td>{day.rain}</td>
+                    <td>{Math.round(day.pop * 100)}%</td>
+                    <td>{day.theSunrise[0]}</td>
+                    <td>{day.theSunset}</td>
+                    <td>{day.theMoonrise}</td>
+                    <td>{day.theMoonset}</td>
+                    <td>{Math.round(day.moon_phase * 100)}%</td>
+                  </tr>
+                );
+              } else if (screenSize.dynamicWidth > 600) {
+                return (
+                  <tr style={{ color: 'white' }}>
+                    <td>{day.theDay}</td>
+                    <td>{(day.temp.max - 273.15).toFixed(1)}&#176;</td>
+                    <td>{(day.temp.min - 273.15).toFixed(1)}&#176;</td>
+                    <td>{day.pressure}</td>
+                    <td>{day.humidity}%</td>
+                    <td>{day.wind_speed}</td>
+                    <td>{day.weather[0].main}</td>
+                    <td>{day.theClouds}</td>
+                    <td>{day.rain}</td>
+                    <td>{Math.round(day.pop * 100)}%</td>
+                    <td>{day.theSunrise[0]}</td>
+                    <td>{day.theSunset}</td>
+                    <td>{Math.round(day.moon_phase * 100)}%</td>
+                  </tr>
+                );
+              } else if (screenSize.dynamicWidth > 400) {
+                return (
+                  <tr style={{ color: 'white' }}>
+                    <td>{day.theDay}</td>
+                    <td>{(day.temp.max - 273.15).toFixed(1)}&#176;</td>
+                    <td>{(day.temp.min - 273.15).toFixed(1)}&#176;</td>
+                    <td>{day.humidity}%</td>
+                    <td>{day.weather[0].main}</td>
+                    <td>{Math.round(day.pop * 100)}%</td>
+                    <td>{day.theSunrise[0]}</td>
+                    <td>{day.theSunset}</td>
+                    <td>{Math.round(day.moon_phase * 100)}%</td>
+                  </tr>
+                );
+              } else {
+                return (
+                  <tr style={{ color: 'white' }}>
+                    <td>{day.theDay}</td>
+                    <td>{(day.temp.max - 273.15).toFixed(1)}&#176;</td>
+                    <td>{(day.temp.min - 273.15).toFixed(1)}&#176;</td>
+                    <td>{day.weather[0].main}</td>
+                    <td>{Math.round(day.pop * 100)}%</td>
+                    <td>{Math.round(day.moon_phase * 100)}%</td>
+                  </tr>
+                );
+              }
             })}
           </tbody>
         </Table>
