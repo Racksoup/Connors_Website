7d73d9ed8b7581d48efb898c6c443afef5010ea3
Racksoup <rack.connor1@gmail.com>
2022-08-18 00:49:20 -0400

    mobile hourly weather view

--- client/src/UI/Pages/Weather/Hourly.jsx
+++ client/src/UI/Pages/Weather/Hourly.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { selectHourly } from '../../../Redux/weatherSlice';
 import './Weather.scss';
 
@@ -7,6 +7,26 @@ import Table from 'react-bootstrap/Table';
 
 const Hourly = () => {
   const hourly = useSelector(selectHourly);
+
+  const [screenSize, getDimension] = useState({
+    dynamicWidth: window.innerWidth,
+    dynamicHeight: window.innerHeight,
+  });
+
+  const setDimension = () => {
+    getDimension({
+      dynamicWidth: window.innerWidth,
+      dynamicHeight: window.innerHeight,
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', setDimension);
+
+    return () => {
+      window.removeEventListener('resize', setDimension);
+    };
+  }, [screenSize]);
 
   const [time, setTime] = useState(
     new Date().toLocaleString('en-US', { timeZone: 'America/New_York' })
@@ -24,16 +44,26 @@ const Hourly = () => {
         <h1 className='Title'>Hourly</h1>
         <Table bordered variant='dark' style={{ width: '80%', margin: 'auto', marginTop: '20px' }}>
           <thead>
-            <tr style={{ color: 'white' }}>
-              <th>Hour</th>
-              <th>Temp</th>
-              <th>Weather</th>
-              <th>Humidity</th>
-              <th>Pressure</th>
-              <th>Wind Speed</th>
-              <th>Clouds</th>
-              <th>Pop</th>
-            </tr>
+            {screenSize.dynamicWidth > 650 ? (
+              <tr style={{ color: 'white' }}>
+                <th>Hour</th>
+                <th>Temp</th>
+                <th>Weather</th>
+                <th>Humidity</th>
+                <th>Pressure</th>
+                <th>Wind Speed</th>
+                <th>Clouds</th>
+                <th>Pop</th>
+              </tr>
+            ) : (
+              <tr style={{ color: 'white' }}>
+                <th>Hour</th>
+                <th>Temp</th>
+                <th>Weather</th>
+                <th>Clouds</th>
+                <th>Pop</th>
+              </tr>
+            )}
           </thead>
           <tbody>
             {hourly.map((hour) => {
@@ -46,21 +76,37 @@ const Hourly = () => {
                 }
               }
               hourTag++;
-              return (
-                <tr style={{ color: 'white' }}>
-                  <td>
-                    {hourTag}
-                    {dayNight}
-                  </td>
-                  <td>{(hour.feels_like - 273.15).toFixed(2)}&#176;</td>
-                  <td>{hour.weather[0].main}</td>
-                  <td>{hour.humidity}%</td>
-                  <td>{hour.pressure}</td>
-                  <td>{hour.wind_speed}</td>
-                  <td>{hour.clouds}%</td>
-                  <td>{Math.round(hour.pop * 100)}%</td>
-                </tr>
-              );
+
+              if (screenSize.dynamicWidth > 650) {
+                return (
+                  <tr style={{ color: 'white' }}>
+                    <td>
+                      {hourTag}
+                      {dayNight}
+                    </td>
+                    <td>{(hour.feels_like - 273.15).toFixed(2)}&#176;</td>
+                    <td>{hour.weather[0].main}</td>
+                    <td>{hour.humidity}%</td>
+                    <td>{hour.pressure}</td>
+                    <td>{hour.wind_speed}</td>
+                    <td>{hour.clouds}%</td>
+                    <td>{Math.round(hour.pop * 100)}%</td>
+                  </tr>
+                );
+              } else {
+                return (
+                  <tr style={{ color: 'white' }}>
+                    <td>
+                      {hourTag}
+                      {dayNight}
+                    </td>
+                    <td>{(hour.feels_like - 273.15).toFixed(2)}&#176;</td>
+                    <td>{hour.weather[0].main}</td>
+                    <td>{hour.clouds}%</td>
+                    <td>{Math.round(hour.pop * 100)}%</td>
+                  </tr>
+                );
+              }
             })}
           </tbody>
         </Table>
