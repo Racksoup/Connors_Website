26d75521585717ee9f7c71289a9fa9288c2cf4c1
racksoup <rack.connor1@gmail.com>
2025-02-28 04:22:12 -0500

    ff

--- client/src/UI/Pages/Weather/Current.jsx
+++ client/src/UI/Pages/Weather/Current.jsx
@@ -4,7 +4,6 @@ import { selectCurrent } from '../../../Redux/weatherSlice';
 
 import { useSelector } from 'react-redux';
 
-
 const Current = () => {
   const currentWeather = useSelector(selectCurrent);
 
@@ -12,9 +11,20 @@ const Current = () => {
   
   if (currentWeather)
     return <div className='Current'>
-      <h3 className='Date'>{currentWeather.dt}</h3>
+      <h3 className='Date'>
+        <p>{new Date(currentWeather.dt * 1000).toLocaleString('en-us',{month:'long'})}</p>
+        <p>&nbsp;</p>
+        <p>{new Date(currentWeather.dt * 1000).getDate()}</p>
+      </h3>
       <div className='Temp'>
-
+        <div className='Left'>
+          <p> Current </p>
+          <p className='Val'>{Math.floor((currentWeather.temp - 273.15) *100) /100}</p>
+        </div>
+        <div className='Right'>
+          <p> Feel </p>
+          <p className='Val'>{Math.floor((currentWeather.feels_like - 273.15) *100) /100}</p>
+        </div>
       </div>
       <div className='Info'>
 
@@ -23,3 +33,6 @@ const Current = () => {
 }
 
 export default Current;
+
+
+
